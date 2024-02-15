1fec238a52fa4bb7349aa02da0ea0567370c6a81
Administrator <Administrator@BLRTRAINING1.COM>
2024-02-15 17:12:03 +0530

    gh-pages install with some changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className='w-100 nav-container bg-secondary-subtle position-fixed'>
         <div className='container mx-auto h-100 d-flex justify-content-between align-items-center '>
-          <h4 className='appName m-0 p-0'><a href= "/">Shoppikkkng Cart</a></h4>
+          <h4 className='appName m-0 p-0'><a href= "/">Shopping Cart</a></h4>
              
           <ul className='links d-flex justify-content-start m-0 p-0 list-unstyled'>
               <li className='nav-item mx-2'>
